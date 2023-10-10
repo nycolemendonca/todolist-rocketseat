333957ac11f881d5853d858160af956b93cc28c6
nycolemendonca <nycolemendonca4@gmail.com>
2023-10-10 13:16:19 -0300

    Aula 01 - Construção do Back-End

---
+++ todolist/src/main/java/br/rockectseat/todolist/user/UserModel.java
@@ -0,0 +1,13 @@
+package br.rockectseat.todolist.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserModel {
+    private String name;
+    private String username;
+    private String password;
+
+}
